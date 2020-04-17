992c72c85a558319a2bf702c151be37f0c810581
xenahort <juan.carlos.wow.95@gmail.com>
2020-04-17 20:00:59 +0200

    Rest ArrayList test

--- Initial/src/main/java/com/example/controllers/GreetingController.java
+++ Initial/src/main/java/com/example/controllers/GreetingController.java
@@ -1,5 +1,6 @@
-package com.example.controller;
+package com.example.controllers;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restservice.Greeting;
 
+/**
+ * Example controller class to test stupid things
+ * @author jserrape
+ *
+ */
 @RestController
 public class GreetingController {
 
@@ -17,5 +23,18 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/listGreetings")
+	public ArrayList<Greeting> greeting() {
+		Greeting gr1, gr2, gr3;
+		gr1 = new Greeting(counter.incrementAndGet(),"a");
+		gr2 = new Greeting(counter.incrementAndGet(),"b");
+		gr3 = new Greeting(counter.incrementAndGet(),"c");
+		ArrayList<Greeting> greetings = new ArrayList<>();
+		greetings.add(gr1);
+		greetings.add(gr2);
+		greetings.add(gr3);
+		return greetings;
 	}
 }
